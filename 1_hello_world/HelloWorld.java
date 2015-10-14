c7ac27c0199304ba3c28e9072d5e5cdda90a34d4
Nigel Gregoire <nigelgregoire@gmail.com>
2015-10-14 22:26:29 +0200

    Add HelloWorld

---
+++ 1_hello_world/HelloWorld.java
@@ -0,0 +1,12 @@
+/******************************************************************************
+ * To run
+ *	$ javac HelloWorld.java
+ *	$ java HelloWorld
+ ******************************************************************************/
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World");
+    }
+}
+
